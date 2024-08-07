67d5e7eb33ae6b0bcb08127d41d0c23e1c3e2f16
muscle-developer <sintyoung5683@naver.com>
2024-08-07 10:58:57 +0900

    인게임 컨트롤러 추가, 매니저 로드 씬 적용

--- Snake Project/Assets/Controller/IngameController.cs
+++ Snake Project/Assets/Controller/IngameController.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 개별 객체 제어: 특정 객체나 캐릭터의 행동과 동작을 제어합니다.
+// 입력 처리: 플레이어의 입력을 받아 해당 객체가 어떻게 반응할지를 결정합니다.
+// 객체 중심: 주로 개별 게임 객체에 붙어서 동작하며, 해당 객체의 움직임, 애니메이션, 상호작용 등을 관리합니다.
 public class IngameController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject snakeHeadPrefab; // SnakeHead 프리팹
+
     void Start()
     {
-        
+        InstantiateSnakeHead();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void InstantiateSnakeHead()
     {
-        
+        GameObject snakeHead = Instantiate(snakeHeadPrefab, transform.position, transform.rotation, transform);
+        TestManager.Instance.snakeBody.Add(snakeHead);
     }
 }
