3e0b380f0676e999573c43ea07e3fe8297b1f3de
muscle-developer <sintyoung5683@naver.com>
2024-08-21 11:23:37 +0900

    게임매니저 생성 및 프리팹 추가

--- Snake Project/Assets/Scripts/Managers/GameManager.cs
+++ Snake Project/Assets/Scripts/Managers/GameManager.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static GameManager Instace;
 
-    // Update is called once per frame
-    void Update()
+    public void Awake()
     {
-        
+        GameManager.Instace = this;
     }
 }
