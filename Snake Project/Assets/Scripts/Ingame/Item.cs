d853e1f62953292a226551137bee536cebf9de38
muscle-developer <sintyoung5683@naver.com>
2024-09-21 17:55:00 +0900

    충돌 로직 처리중

--- Snake Project/Assets/Scripts/Ingame/Item.cs
+++ Snake Project/Assets/Scripts/Ingame/Item.cs
@@ -5,8 +5,15 @@ using UnityEngine;
 public class Item : MonoBehaviour
 {
     public ItemManager.ItemType itemtype;
-    void Start()
+
+    // 플레이어와 충돌 시 아이템의 효과를 적용하는 메서드
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player")) // 플레이어와 충돌했는지 확인
+        {
+            // ApplyItemEffect(other.gameObject); // 효과 적용
+            gameObject.SetActive(false);
+            // Destroy(gameObject); // 아이템을 사용 후 파괴
+        }
     }    
 }   
