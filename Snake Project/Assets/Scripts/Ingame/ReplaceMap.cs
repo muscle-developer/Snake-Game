b09072efe36f5fca9326eb465f209d1e17d4fcde
muscle-developer <sintyoung5683@naver.com>
2024-09-23 18:00:48 +0900

    맵 재배치를 할경우

--- Snake Project/Assets/Scripts/Ingame/ReplaceMap.cs
+++ Snake Project/Assets/Scripts/Ingame/ReplaceMap.cs
@@ -10,18 +10,18 @@ public class ReplaceMap : MonoBehaviour
         Vector3 playerPos = GameManager.Instance.player.transform.position;
         Vector3 mapPos = this.transform.position;
 
+        float diffX = playerPos.x - mapPos.x;
+        float diffZ = playerPos.z - mapPos.z;
+
+        float dirX = diffX < 0 ? -1 : 1;
+        float dirZ = diffZ < 0 ? -1 : 1;
+
+        diffX = Mathf.Abs(diffX);
+        diffZ = Mathf.Abs(diffZ);
+
         switch(transform.tag)
         {
             case "Floor":
-                float diffX = playerPos.x - mapPos.x;
-                float diffZ = playerPos.z - mapPos.z;
-
-                float dirX = diffX < 0 ? -1 : 1;
-                float dirZ = diffZ < 0 ? -1 : 1;
-
-                diffX = Mathf.Abs(diffX);
-                diffZ = Mathf.Abs(diffZ);
-
                 if (diffX > diffZ)
                     transform.Translate(Vector3.right * dirX * 40);
                 else if (diffX < diffZ)
