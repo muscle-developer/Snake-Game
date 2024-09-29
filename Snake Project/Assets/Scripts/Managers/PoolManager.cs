1c2b444a10614f1f711daa7adaf1c9c0a11c6859
muscle-developer <sintyoung5683@naver.com>
2024-09-29 21:23:09 +0900

    풀매니저 싱글톤화

--- Snake Project/Assets/Scripts/Managers/PoolManager.cs
+++ Snake Project/Assets/Scripts/Managers/PoolManager.cs
@@ -1,18 +1,13 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PoolManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public static PoolManager Instance;
 
-    // Update is called once per frame
-    void Update()
+
+    void Awake()
     {
-        
+        Instance = this;
     }
 }
