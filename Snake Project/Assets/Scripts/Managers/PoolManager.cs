9cdcf9be080c6edc8d7189e0d53dfdb775c640c2
muscle-developer <sintyoung5683@naver.com>
2024-09-30 19:21:46 +0900

    아이템 풀링 진행중

--- Snake Project/Assets/Scripts/Managers/PoolManager.cs
+++ Snake Project/Assets/Scripts/Managers/PoolManager.cs
@@ -4,10 +4,26 @@ using UnityEngine;
 public class PoolManager : MonoBehaviour
 {
     public static PoolManager Instance;
-
+    private Dictionary<string, Queue<GameObject>> poolDictionary = new Dictionary<string, Queue<GameObject>>();
 
     void Awake()
     {
         Instance = this;
+    }
+
+    public void CreatePool()
+    {
+
+    }
+
+    public GameObject GetFromPool()
+    {
+        return gameObject;
+    }
+
+    // 오브젝트를 풀로 반환하기
+    public void ReturnToPool()
+    {
+
     }
 }
