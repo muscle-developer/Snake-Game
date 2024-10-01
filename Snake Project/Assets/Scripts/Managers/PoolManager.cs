4bdff017f9930bf064a3b17842f9bf41bc10e697
muscle-developer <sintyoung5683@naver.com>
2024-10-01 18:54:31 +0900

    풀매니저 스크립트 수정중

--- Snake Project/Assets/Scripts/Managers/PoolManager.cs
+++ Snake Project/Assets/Scripts/Managers/PoolManager.cs
@@ -11,19 +11,54 @@ public class PoolManager : MonoBehaviour
         Instance = this;
     }
 
-    public void CreatePool()
+    // 특정 프리팹에 대한 풀 생성
+    public void CreatePool(GameObject prefab, int poolSize)
     {
-
+        string prefabName = prefab.name;
+        if (!poolDictionary.ContainsKey(prefabName))
+        {
+            poolDictionary[prefabName] = new Queue<GameObject>();
+            for (int i = 0; i < poolSize; i++)
+            {
+                GameObject itemObj = Instantiate(prefab);
+                itemObj.SetActive(false);
+                poolDictionary[prefabName].Enqueue(itemObj);
+            }
+        }
     }
 
-    public GameObject GetFromPool()
+    // 풀에서 오브젝트 가져오기
+    public GameObject GetFromPool(GameObject prefab, Vector3 position, Quaternion rotation)
     {
-        return gameObject;
+        string prefabName = prefab.name;
+        if (poolDictionary.ContainsKey(prefabName) && poolDictionary[prefabName].Count > 0)
+        {
+            GameObject itemObj = poolDictionary[prefabName].Dequeue();
+            itemObj.SetActive(true);
+            itemObj.transform.position = position;
+            itemObj.transform.rotation = rotation;
+            return itemObj;
+        }
+        else
+        {
+            // 풀에 더 이상 오브젝트가 없으면 새로 생성
+            GameObject itemObj = Instantiate(prefab, position, rotation);
+            return itemObj;
+        }
     }
 
     // 오브젝트를 풀로 반환하기
-    public void ReturnToPool()
+    public void ReturnToPool(GameObject itemObj)
     {
-
+        itemObj.SetActive(false);
+        string prefabName = itemObj.name.Replace("(Clone)", "").Trim();
+        if (poolDictionary.ContainsKey(prefabName))
+        {
+            poolDictionary[prefabName].Enqueue(itemObj);
+        }
+        else
+        {
+            Destroy(itemObj);
+        }
     }
 }
