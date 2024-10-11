92333c053e19fff95e8ee1813e494ba4ea2ee5c5
muscle-developer <sintyoung5683@naver.com>
2024-10-11 18:18:03 +0900

    적 스네이크가 사과 충돌 시 몸통의 갯수가 추가되도록 수정

--- Snake Project/Assets/Scripts/Ingame/Item.cs
+++ Snake Project/Assets/Scripts/Ingame/Item.cs
@@ -6,31 +6,34 @@ public class Item : MonoBehaviour
 {
     public ItemManager.ItemType itemtype;
     private SnakeManager snakeManager;
+    private EnemySnakeManager enemySnakeManager;
 
     // 플레이어와 충돌 시 아이템의 효과를 적용하는 메서드
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Snake Head")) // 플레이어와 충돌했는지 확인
         {
-            ApplyItemEffect(other.gameObject); // 효과 적용
+            ApplyItemEffect(other.gameObject, true); // 효과 적용
         }        
         else if(other.CompareTag("Enemy Snake"))
         {
+            ApplyItemEffect(other.gameObject, false); // 적 스네이크에 대한 효과 적용
         }
     }    
 
     // 아이템 효과 적용 로직
-    private void ApplyItemEffect(GameObject player)
+    private void ApplyItemEffect(GameObject snake, bool isPlayer)
     {
         snakeManager = SnakeManager.Instance;
-        
-        if (snakeManager != null)
+        enemySnakeManager = EnemySnakeManager.Instance;
+
+        if (isPlayer && snakeManager != null)
         {
             switch (itemtype)
             {
                 case ItemManager.ItemType.APPLE:
                     snakeManager.AddBodyPart(); // 사과는 몸체 추가
-                    player.GetComponentInChildren<PlayerCanvas>().LevelUp(); // 레벨업 호출
+                    snake.GetComponentInChildren<PlayerCanvas>().LevelUp(); // 레벨업 호출
                     break;
                 case ItemManager.ItemType.SPEED:
                     snakeManager.ApplySpeedBoost(5f, 5f); // 스피드 부스트
@@ -38,6 +41,15 @@ public class Item : MonoBehaviour
                 case ItemManager.ItemType.MAGNET:
                     snakeManager.ActivateMagnet(5f); // 자석 활성화
                     break;
+            }
+        }
+        else if (!isPlayer && enemySnakeManager != null)
+        {
+            // 적 스네이크가 사과를 먹었을 때 몸체를 추가
+            EnemySnake enemySnake = snake.GetComponent<EnemySnake>();
+            if (enemySnake != null && itemtype == ItemManager.ItemType.APPLE)
+            {
+                enemySnake.AddBodyPart(); // 적 스네이크 몸체 추가
             }
         }
 
