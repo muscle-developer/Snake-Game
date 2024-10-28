6572a1d31551ea3d7e12a7f8c785979c8fcb5e04
muscle-developer <sintyoung5683@naver.com>
2024-10-28 19:25:15 +0900

    로비매니저 코드 추가 - 페이드효과(비동기 씬전환), 닉네임 설정, 게임시작 버튼 추가

--- Snake Project/Assets/Scripts/Managers/LobbyManager.cs
+++ Snake Project/Assets/Scripts/Managers/LobbyManager.cs
@@ -1,7 +1,90 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class LobbyManager : MonoBehaviour
 {
+    [SerializeField] 
+    private Image fadeImage;
+    [SerializeField]
+    private TMP_InputField input_Nickname;
+    [SerializeField]
+    private Button btn_Start;
+
+    private Coroutine co = null;
+    public bool deleteData;
+    private float fadeDuration = 1f;
+
+    private void Awake()
+    {
+        if (deleteData)
+            PlayerPrefs.DeleteAll();
+    }
+
+    private void Start()
+    {
+        fadeImage.gameObject.SetActive(true);
+        StartCoroutine(FadeOut());
+    }
+
+    public void OnInputFieldValueChanged()
+    {
+        btn_Start.interactable = input_Nickname.text.Length > 0;
+    }
+
+    public void OnClickBtnStart()
+    {
+        if (co == null)
+            co = StartCoroutine(SceneTrans("Main Game"));
+    }
+
+    private IEnumerator FadeOut()
+    {
+        Color color = fadeImage.color;
+        float startAlpha = color.a;
+
+        for (float t = 0; t < fadeDuration; t += Time.deltaTime)
+        {
+            float normalizedTime = t / fadeDuration;
+            color.a = Mathf.Lerp(startAlpha, 0f, normalizedTime);
+            fadeImage.color = color;
+            yield return null;
+        }
+
+        color.a = 0f;
+        fadeImage.color = color;
+    }
+
+    private IEnumerator FadeIn()
+    {
+        Color color = fadeImage.color;
+        float startAlpha = color.a;
+
+        for (float t = 0; t < fadeDuration; t += Time.deltaTime)
+        {
+            float normalizedTime = t / fadeDuration;
+            color.a = Mathf.Lerp(startAlpha, 1f, normalizedTime);
+            fadeImage.color = color;
+            yield return null;
+        }
+
+        color.a = 1f;
+        fadeImage.color = color;
+    }
+
+    private IEnumerator SceneTrans(string sceneName)
+    {
+        input_Nickname.interactable = false;
+        PlayerPrefs.SetString("Nickname", input_Nickname.text);
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(sceneName);
+        async.allowSceneActivation = false;
+
+        yield return StartCoroutine(FadeIn());
+        async.allowSceneActivation = true;
+        
+        co = null;
+    }
 }
