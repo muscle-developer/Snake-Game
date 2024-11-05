a3d3817eea464c8715307a9c3f275399115199ce
muscle-developer <sintyoung5683@naver.com>
2024-11-05 16:20:47 +0900

    게임매니저에서 시간 수정

--- Snake Project/Assets/Scripts/Managers/GameManager.cs
+++ Snake Project/Assets/Scripts/Managers/GameManager.cs
@@ -12,26 +12,23 @@ public class GameManager : MonoBehaviour
     public bool isLive;
     
     // 게임 시간 (초)
-    private float gameTime = 60;
-    // 남은 게임 시간 텍스트
-    [SerializeField]
-    private TMP_Text timeText;
+    public float gameTime = 60;
 
 
     public void Awake()
     {
         GameManager.Instance = this;
         Init();
+        isLive = true;
     }
 
-    void Start()
+    private void Init()
     {
-        int min = Mathf.FloorToInt(gameTime / 60);
-        int sec = Mathf.FloorToInt(gameTime % 60);
-        timeText.text = string.Format("{0:D2}:{1:D2}", min, sec);
+        // rankScore = PlayerPrefs.GetInt("rankScore");
+        // text_RankScore.text = rankScore.ToString();
     }
-
-    void Update()
+    
+    private void Update()
     {
         if (!isLive)
             return;
@@ -43,16 +40,10 @@ public class GameManager : MonoBehaviour
         }
     }
 
-    private void Init()
-    {
-        // rankScore = PlayerPrefs.GetInt("rankScore");
-        // text_RankScore.text = rankScore.ToString();
-    }
-    
-
     // 게임 오버 처리
     public void GameOver()
     {
+        isLive = false;
         Debug.Log("Game Over! Player's snake has been destroyed.");
         // 여기에서 게임 오버 UI 표시나 리스타트 기능 등을 추가할 수 있습니다.
         // 예: UIManager.Instance.ShowGameOverScreen();
