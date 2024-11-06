3e7a813a5e0cdba0e0f27162c1db9924ae94bf68
muscle-developer <sintyoung5683@naver.com>
2024-11-06 15:07:14 +0900

    UI 스코어 업데이트 함수 추가

--- Snake Project/Assets/Scripts/UI/UIViewMain.cs
+++ Snake Project/Assets/Scripts/UI/UIViewMain.cs
@@ -1,12 +1,24 @@
 // UIViewMain.cs
 using UnityEngine;
 using TMPro;
+using System.Collections.Generic;
 
 public class UIViewMain : MonoBehaviour
 {
     // 남은 게임 시간 텍스트
     [SerializeField]
     private TMP_Text timeText;
+
+    // 스코어 점수
+    [SerializeField]
+    private List<TMP_Text> scoreTextList = new List<TMP_Text>();
+    // 적 스코어를 저장할 리스트
+    private List<int> enemyScores = new List<int>();
+
+    void Start()
+    {
+        UpdateScoreUI();
+    }
 
     void LateUpdate()
     {
@@ -17,5 +29,31 @@ public class UIViewMain : MonoBehaviour
         int min = Mathf.FloorToInt(gameTime / 60);
         int sec = Mathf.FloorToInt(gameTime % 60);
         timeText.text = string.Format("{0:D2}:{1:D2}", min, sec);
+    }
+
+    // 점수를 업데이트하는 메서드
+    public void UpdateScoreUI()
+    {
+        // 플레이어 점수 업데이트
+        int playerScore = SnakeManager.Instance.BodyParts.Count;
+        scoreTextList[0].text = "Player Score :" + playerScore.ToString();
+
+        // 적 점수 업데이트
+        UpdateEnemyScores();
+        for (int i = 0; i < enemyScores.Count; i++)
+        {
+            scoreTextList[i + 1].text = "Enemy Score : " + enemyScores[i].ToString(); // 적 점수를 UI에 표시
+        }
+    }
+
+    // 적 점수 업데이트 메서드
+    private void UpdateEnemyScores()
+    {
+        enemyScores.Clear(); // 이전 점수 초기화
+        foreach (var enemy in EnemySnakeManager.Instance.enemySnakes) // EnemySnakeManager에 있는 모든 적 스네이크를 조회
+        {
+            int enemyScore = enemy.bodyParts.Count; // 적 스코어는 몸체 개수로 계산
+            enemyScores.Add(enemyScore);
+        }
     }
 }
