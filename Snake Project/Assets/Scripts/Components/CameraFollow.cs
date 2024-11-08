06dc8915181d2a6c4639eb0a478f1ef7f3d8406d
muscle-developer <sintyoung5683@naver.com>
2024-11-08 11:10:29 +0900

    블로그에 올리기 위한 카메라 시점 스위칭

--- Snake Project/Assets/Scripts/Components/CameraFollow.cs
+++ Snake Project/Assets/Scripts/Components/CameraFollow.cs
@@ -21,6 +21,7 @@ public class CameraFollow : MonoBehaviour
         while (target == null)
         {
             target = FindObjectOfType<SnakeHead>()?.transform; // ?.는 널 조건부 연산자로, FindObjectOfType<SnakeHead>()가 null인 경우 안전하게 target을 null로 설정합니다.
+            // target = FindObjectOfType<EnemySnake>()?.transform; // ?.는 널 조건부 연산자로, FindObjectOfType<SnakeHead>()가 null인 경우 안전하게 target을 null로 설정합니다.
             yield return null;
         }
         
