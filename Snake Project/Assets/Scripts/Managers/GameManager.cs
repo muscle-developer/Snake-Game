afe397d5bc4ad0b07bb4fa5c694bbd02108e99da
muscle-developer <sintyoung5683@naver.com>
2024-11-14 19:52:16 +0900

    새로운게임, 이어하기, 다음단계 구분해서 관리

--- Snake Project/Assets/Scripts/Managers/GameManager.cs
+++ Snake Project/Assets/Scripts/Managers/GameManager.cs
@@ -1,7 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -11,6 +9,9 @@ public class GameManager : MonoBehaviour
 
     [Header("게임 관련")]
     public bool isLive;
+    public bool isNewGame = false; // 새로 시작한 경우
+    public bool isNextGame = false; // 성공 후 다음으로 넘어가는 게임
+    public bool isCurrentGame = false; // 목표점수를 달성 못해서 재도전 게임
     
     // 게임 시간 (초)
     public float gameTime = 60;
@@ -24,9 +25,58 @@ public class GameManager : MonoBehaviour
         isLive = true;
     }
 
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name == "Main Game")
+        {
+            // 각 상태가 이미 설정된 상태를 유지
+            if (isCurrentGame)
+            {
+                Debug.Log("현재 게임 유지 중");
+                // isCurrentGame 유지, 다른 상태는 초기화하지 않음
+                isLive = true;
+            }
+            else if (isNextGame)
+            {
+                Debug.Log("다음 게임 상태 유지 중");
+                // isNextGame 유지, 다른 상태는 초기화하지 않음
+                isLive = true;
+            }
+            else if (isNewGame)
+            {
+                Debug.Log("새로운 게임 시작 중");
+                // 새로운 게임 설정, isNewGame만 true 유지
+                isNewGame = true;
+                isLive = true;
+            }
+            else
+            {
+                // 만약 모든 조건이 충족되지 않으면 기본 설정
+                Debug.Log("기본 새 게임 설정");
+                isNewGame = true;
+                isCurrentGame = false;
+                isNextGame = false;
+                isLive = true;
+            }
+        }
+    }
+
     private void Init()
     {
         initialGameTime = gameTime; // 게임 시작 시간을 초기화 시간으로 설정
+        isNewGame = false;
+        isNextGame = false;
+        isCurrentGame = false;
     }
 
     private void Update()
@@ -54,6 +104,7 @@ public class GameManager : MonoBehaviour
     {
         isLive = true;
         gameTime = initialGameTime; // 게임 시간을 초기화
+        isNewGame = false; // 게임 리셋 시 새로운 게임 상태를 false로 설정
         mainCanvs.ResetGame(); // UIViewMain의 UI 초기화 메서드 호출
     }
 }
