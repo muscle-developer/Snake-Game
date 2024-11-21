ecb746eab2ca9ef793955186f9fa33f7509bb3bb
muscle-developer <sintyoung5683@naver.com>
2024-11-21 15:54:21 +0900

    게임 상태를 확인 하기 위한 스크립트

---
+++ Snake Project/Assets/Scripts/GameState/IGameState.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// IGameState.cs
+public interface IGameState
+{
+    void EnterState(GameManager gameManager);  // 상태에 진입할 때 실행
+    void UpdateState(GameManager gameManager); // 매 프레임 실행
+    void ExitState(GameManager gameManager);   // 상태에서 나갈 때 실행
+}
+
