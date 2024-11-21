685e9a0b6c898f0e359842abceefb83b60803a54
muscle-developer <sintyoung5683@naver.com>
2024-11-21 15:54:48 +0900

    게임 상태를 설정하기위한 메서드 추가

---
+++ Snake Project/Assets/Scripts/GameState/NewGameState.cs
@@ -0,0 +1,104 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewGameState : IGameState
+{
+    public void EnterState(GameManager gameManager)
+    {
+        Debug.Log("Entering New Game State");
+        gameManager.isNewGame = true;   // 새로운 게임 상태로 설정
+        gameManager.isCurrentGame = false;
+        gameManager.isNextGame = false;
+        gameManager.isLive = true;
+
+        gameManager.gameTime = 60;
+    }
+
+    public void UpdateState(GameManager gameManager)
+    {
+        // NewGame 상태에서 필요한 업데이트 로직
+        if (gameManager.gameTime <= 0)
+        {
+            gameManager.SetState(new GameOverState());  // 게임 오버 상태로 전환
+        }
+    }
+
+    public void ExitState(GameManager gameManager)
+    {
+        Debug.Log("Exiting New Game State");
+    }
+}
+
+public class NextGameState : IGameState
+{
+    public void EnterState(GameManager gameManager)
+    {
+        Debug.Log("Entering Next Game State");
+        gameManager.isNextGame = true;
+        gameManager.isNewGame = false;
+        gameManager.isCurrentGame = false;
+        gameManager.isLive = true;
+    }
+
+    public void UpdateState(GameManager gameManager)
+    {
+        if (gameManager.gameTime <= 0)
+        {
+            gameManager.SetState(new GameOverState());
+        }
+    }
+
+    public void ExitState(GameManager gameManager)
+    {
+        Debug.Log("Exiting Next Game State");
+    }
+}
+
+public class CurrentGameState : IGameState
+{
+    public void EnterState(GameManager gameManager)
+    {
+        Debug.Log("Entering Current Game State");
+        gameManager.isCurrentGame = true;
+        gameManager.isNewGame = false;
+        gameManager.isNextGame = false;
+        gameManager.isLive = true;
+    }
+
+    public void UpdateState(GameManager gameManager)
+    {
+        if (gameManager.gameTime <= 0)
+        {
+            gameManager.SetState(new GameOverState());
+        }
+    }
+
+    public void ExitState(GameManager gameManager)
+    {
+        Debug.Log("Exiting Current Game State");
+    }
+}
+
+public class GameOverState : IGameState
+{
+    public void EnterState(GameManager gameManager)
+    {
+        Debug.Log("Entering Game Over State");
+        gameManager.isLive = false;
+        gameManager.isCurrentGame = false;
+        gameManager.isNewGame = false;
+        gameManager.isNextGame = false;
+        gameManager.mainCanvs.GameoverUI();
+    }
+
+    public void UpdateState(GameManager gameManager)
+    {
+        // Game Over 상태에서는 추가 업데이트 로직 없음
+    }
+
+    public void ExitState(GameManager gameManager)
+    {
+        Debug.Log("Exiting Game Over State");
+    }
+}
